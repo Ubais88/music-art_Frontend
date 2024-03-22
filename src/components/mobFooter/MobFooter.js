a7ec5da6de7406634288a5050fc3cbb7b2df707f
Ubais88 <moubais2143@gmail.com>
2024-03-22 11:14:11 +0530

    mobile footer added

--- src/components/mobFooter/MobFooter.js
+++ src/components/mobFooter/MobFooter.js
@@ -2,23 +2,34 @@ import React from "react";
 import { GoHome } from "react-icons/go";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
 import loginUser from "../../assets/loginuser.png";
+import loginVector from "../../assets/loginVector.png";
 import styles from "./MobFooter.module.css";
+import { CiUser } from "react-icons/ci";
 
 const MobFooter = () => {
   return (
     <div className={styles.container}>
       <div className={styles.footerItem}>
-        <GoHome color="#2E0052" size={30}/>
+        <GoHome color="#2E0052" size={30} />
         <span>Home</span>
       </div>
       <div className={styles.footerItem}>
-        <div>
+        <div className={styles.cartContainer}>
           <MdOutlineAddShoppingCart color="#2E0052" size={30} />
+          <span className={styles.cartQuantity}>12</span>
         </div>
         <span>Cart</span>
       </div>
       <div className={styles.footerItem}>
-        <img src={loginUser} alt="mobilefooter" className={styles.userIcon} />
+        {true ? (
+          <img
+            src={loginVector}
+            alt="mobilefooter"
+            className={styles.userIcon}
+          />
+        ) : (
+          <img src={loginUser} alt="mobilefooter" className={styles.userIcon} />
+        )}
         <span>Login</span>
       </div>
     </div>

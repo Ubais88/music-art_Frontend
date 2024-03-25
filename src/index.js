ee5d209621aab312e13fb1b5f9cb06c39a9a01d0
Ubais88 <moubais2143@gmail.com>
2024-03-25 10:29:53 +0530

    store added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import './index.css'
+import { AuthProvider } from "./store/auth";
+
+const toastOptions = {
+  position: "top-right",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-    <Toaster />
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+      <Toaster {...toastOptions} />
+    </AuthProvider>
+  </BrowserRouter>
 );
